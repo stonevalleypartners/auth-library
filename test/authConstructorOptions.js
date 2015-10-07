8a506856dd260330866286649aa6bfc604f34538
J. Fritz Barnes <fritz@stonevalleypartners.com>
2015-10-07 11:39:56 -0700

    add more tests
    * test bearer authentication
    * test option parameters passed to constructor

---
+++ test/authConstructorOptions.js
@@ -0,0 +1,72 @@
+var lodash = require('lodash');
+var Auth = require('../lib').Auth;
+
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+chai.should();
+var getLogger = require('./lib/getLogger');
+
+var log = getLogger('constructor');
+var allOpts = {
+  log: log,
+  secret: 'auth constructor tests',
+  tokenDuration: 1800,
+  maxRefreshTokens: 2,
+  lookupAccount: lodash.noop,
+  registerExternalAccount: lodash.noop,
+  updateTokens: lodash.noop,
+  updateAccount: lodash.noop,
+};
+
+describe('auth constructor options', () => {
+  it('valid construction with all options specified', () => {
+    var a = new Auth(allOpts);
+    a.opts.tokenDuration.should.equal(1800);
+    a.opts.maxRefreshTokens.should.equal(2);
+  });
+
+  it('valid construction using tokenDuration default', () => {
+    var opts = lodash.clone(allOpts);
+    delete opts.tokenDuration;
+
+    var a = new Auth(opts);
+    a.opts.tokenDuration.should.equal(3600);
+    a.opts.maxRefreshTokens.should.equal(2);
+  });
+
+  it('valid construction using maxRefreshTokens default', () => {
+    var opts = lodash.clone(allOpts);
+    delete opts.maxRefreshTokens;
+
+    var a = new Auth(opts);
+    a.opts.tokenDuration.should.equal(1800);
+    a.opts.maxRefreshTokens.should.equal(5);
+  });
+
+  it('invalid construction missing secret', () => {
+    var opts = lodash.clone(allOpts);
+    delete opts.secret;
+
+    return new Promise((resolve) => {
+      resolve(new Auth(opts));
+    })
+    .should.eventually.be.rejectedWith('opts.secret required');
+  });
+
+  var requiredFields = ['log', 'secret', 'lookupAccount', 'updateTokens', 'updateAccount'];
+  for (var field of requiredFields) {
+    testMissingField(field);
+  }
+});
+
+function testMissingField(field) {
+  it('invalid construction missing ' + field, () => {
+    var opts = lodash.clone(allOpts);
+    delete opts[field];
+
+    return new Promise((resolve) => {
+      resolve(new Auth(opts));
+    })
+    .should.eventually.be.rejectedWith('opts.' + field + ' required');
+  });
+}
--- test/encryptHelpers.js
+++ test/encryptHelpers.js
@@ -1,5 +1,5 @@
-'use strict';
 var lodash = require('lodash');
+var crypto = require('crypto');
 var Auth = require('../lib').Auth;
 
 var chai = require('chai');
@@ -28,5 +28,26 @@ describe('encryptHelpers', () => {
   it('decrypt object', () => {
     var decrypted = auth.decryptObject(encryptedString);
     decrypted.should.deep.equal(original);
+  });
+
+  it('decrypt random string should fail', () => {
+    return new Promise((resolve) => {
+      resolve(auth.decryptObject('has not been encrypted'));
+    })
+    .should.eventually.be.rejectedWith('unable to decrypt object');
+  });
+
+  it('decrypt non-object should fail', () => {
+    // manually construct encrypted string
+    var buf = new Buffer(opts.secret);
+    var cipher = crypto.createCipher('aes-256-cbc', buf);
+    var encodedText = cipher.update('a string and not an object', 'utf8', 'base64');
+    encodedText += cipher.final('base64');
+    encodedText = encodedText.replace(/\+/g, '-');
+    encodedText = encodedText.replace(/\//g, '_');
+    return new Promise((resolve) => {
+      resolve(auth.decryptObject(encodedText));
+    })
+    .should.eventually.be.rejectedWith('cannot convert to object');
   });
 });
